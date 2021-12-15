057ac87efa439f51f312f713eb523748b5f92a48
Ashfahan <me@ashfahan.com>
2021-12-15 05:13:12 +0500

    style: fix header UI

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,91 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Headroom from "headroom.js";
 
 import logo from "../assets/main-logo.svg";
 import ast1 from "../assets/ast/1.png";
 import ast2 from "../assets/ast/2.png";
 import ast3 from "../assets/ast/3.png";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 export default function Header() {
+  useEffect(() => {
+    var header = document.querySelector("#navbar");
+    var headroom = new Headroom(header, {
+      classes: { notTop: "bg-white" },
+    });
+    headroom.init();
+  }, []);
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <header>
-      <div className="container " id="container-custom-nav">
-        <nav className="navbar  navbar-expand-lg navbar-light  " id="navbar">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt="" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item ">
-                <a className="nav-link nav-link-ltr" href="#Features">
-                  Features <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#stake">
-                  Staking
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#portfolio"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link "
-                  href="#staking"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link "
-                  href="#contact"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-            <div className="nav-icons my-2 my-lg-0">
-              <ul>
-                <li>
-                  <img src={ast1} />
-                </li>
-                <li>
-                  <img src={ast2} />
-                </li>
-                <li>
-                  <img src={ast3} />
-                </li>
-              </ul>
-              <div className="btn">Dashboard</div>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <Navbar className="container sticky-top" expand="xl" light id="navbar">
+      <NavbarBrand href="/">
+        <img src={logo} alt="" />
+      </NavbarBrand>
+      <NavbarToggler onClick={() => setOpen(!open)} />
+      <Collapse navbar isOpen={open}>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="#Features">Features</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#stage">Staking</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#portfolio">Portfolio</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#staking">FAQs</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#contact">Contact</NavLink>
+          </NavItem>
+        </Nav>
+        <div className="nav-icons my-2 my-lg-0">
+          <ul>
+            <li>
+              <img src={ast1} />
+            </li>
+            <li>
+              <img src={ast2} />
+            </li>
+            <li>
+              <img src={ast3} />
+            </li>
+          </ul>
+          <div className="btn">Dashboard</div>
+        </div>
+      </Collapse>
+    </Navbar>
   );
 }
