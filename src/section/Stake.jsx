250596cd2d51258a5e813511463e1e3ba6e333a7
Ashfahan <me@ashfahan.com>
2021-12-16 03:48:32 +0500

    revamp stake

--- src/section/Stake.jsx
+++ src/section/Stake.jsx
@@ -12,119 +12,154 @@ import car8 from "../assets/ast/car8.png";
 
 export default function Stake() {
   return (
-    <section class="stake-wrapper reveal " id="stake">
-      <img class="bg-icons2" src={boxs} alt="" />
-      <div class="container">
-        <div class="main-content2" data-animate-in="up">
-          <div class="work-heading text-center">
-            <h1>Stake with us</h1>
-            <p class="mt-3">
-              Earn passive income in crypto by delegating your idle tokens. We
-              contribute to secure the following blockchains.
-            </p>
-          </div>
+    <section className="mt-24 overflow-x-hidden">
+      <div className="container-lg relative">
+        <img
+          className="absolute top-0 left-0 -z-10 w-96 transform -translate-x-2/4"
+          src={boxs}
+        />
 
-          <div class="card-group card-group-wrapper ">
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car1} class="mr-1" alt="..." />
-                <div class="media-body  tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+        <div className="container-lg pt-36">
+          <div className="rounded-2xl bg-slate-200 bg-opacity-60 backdrop-blur-sm p-16">
+            <div className="text-center">
+              <h5 className="text-5xl font-bold text-primary">Stake with us</h5>
+              <p className="text-primary-dark font-medium mt-6 mb-12">
+                Earn passive income in crypto by delegating your idle tokens. We
+                contribute to secure the following blockchains.
+              </p>
+            </div>
+
+            <div className="row">
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car1} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
+
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car2} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car2} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car3} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car3} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
-
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car4} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car4} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake "> Stake</button>
-            </div>
-          </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car5} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-          <div class="card-group card-group-wrapper ">
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car5} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car6} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car6} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car7} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car7} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
 
-              <button class="btn-stake"> Stake</button>
-            </div>
+              <div className="col w-3/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg p-3 bg-[#F8FAFD] border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="row no-gutters">
+                    <div className="col w-14 bg-white rounded-lg">
+                      <img src={car8} />
+                    </div>
+                    <div className="col px-2">
+                      <div>Fantom (FTM)</div>
+                      <div className="font-bold text-3xl">15.00%</div>
+                    </div>
+                  </div>
 
-            <div class=" card card-2 text-center ">
-              <div class="media">
-                <img src={car8} class="mr-1" alt="..." />
-                <div class="media-body tabs-body-text">
-                  Fantom (FTM)
-                  <h5 class="mt-0">15.00%</h5>
+                  <button className="btn is-primary w-full mt-2">Stake</button>
                 </div>
               </div>
-
-              <button class="btn-stake "> Stake</button>
             </div>
-          </div>
-          <div class="work-heading mt-4 text-center">
-            <div class="btn-view"> View More</div>
+            <div className="work-heading mt-4 text-center">
+              <div className="btn-view"> View More</div>
+            </div>
           </div>
         </div>
       </div>
