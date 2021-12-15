971af2bdd04b0935af75d37cd05ddb3a108586f6
Ashfahan <me@ashfahan.com>
2021-12-16 03:17:09 +0500

    revamp work sections

--- src/section/Work.jsx
+++ src/section/Work.jsx
@@ -7,55 +7,62 @@ import astsettings from "../assets/ast/settings.png";
 
 export default function Work() {
   return (
-    <section class="work-wrapper" id="Features">
-      <img class="bg-icons" src={workbg} alt="" />
-      <div class="container reveal">
-        <div class="main-content1" data-animate-in="up">
-          <div class="work-heading pt-4 text-center">
-            <h1>Work with us</h1>
-            <p class="mt-3">
-              Here are some reasons why you will make the best decision by
-              choosing us.
-            </p>
-          </div>
-          <div class="card-group card-group-wrapper ">
-            <div class="card card-1 text-center ">
-              <img src={asthands} class="card-img-top pt-4 card-custom-top" />
-              <div class="card-body text-center">
-                <h5 class="card-title card-title-text1">
-                  Strategic partnership
-                </h5>
-                <p class="card-text card-inner-text">
-                  Qube partners with promising blockchain projects, assisting
-                  them towards successful ICOs, through funding, advisory
-                  services and community awareness generation.
-                </p>
-              </div>
+    <section className="mt-24 overflow-x-hidden">
+      <div className="container-lg relative">
+        <img
+          className="bg-icons transform translate-x-2/4"
+          src={workbg}
+          alt=""
+        />
+        <div className="container-lg pt-36">
+          <div className="rounded-2xl bg-slate-200 bg-opacity-60 backdrop-blur-sm p-16">
+            <div className="text-center">
+              <h5 className="text-5xl font-bold text-primary">Work with us</h5>
+              <p className="text-primary-dark font-medium mt-6 mb-12">
+                Here are some reasons why you will make the best decision by
+                choosing us.
+              </p>
             </div>
-            <div class="card card-1 ">
-              <img src={astmessage} class="card-img-top pt-4 card-custom-top" />
-              <div class="card-body text-center">
-                <h5 class="card-title card-title-text2">Advisory services</h5>
-                <p class="card-text card-inner-text">
-                  Through years of advising, Qube has accrued significant
-                  expertise in ICO strategy, including: token metrics, token
-                  sale mechanism, token distribution, exchange listings, and
-                  more.
-                </p>
+            <div className="row">
+              <div className="col">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <img src={asthands} className="max-w-[100px] mx-auto" />
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-primary">
+                    Strategic partnership
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Qube partners with promising blockchain projects, assisting
+                    them towards successful ICOs, through funding, advisory
+                    services and community awareness generation.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div class="card card-1 ">
-              <img
-                src={astsettings}
-                class="card-img-top pt-4 card-custom-top"
-              />
-              <div class="card-body text-center">
-                <h5 class="card-title card-title-text3">Influencer Network</h5>
-                <p class="card-text card-inner-text">
-                  Qube has an extensive social-media influencer network that it
-                  utilizes to build a community for its partner blockchain
-                  projects.
-                </p>
+              <div className="col">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <img src={astmessage} className="max-w-[100px] mx-auto" />
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-[#FE7D1E]">
+                    Advisory services
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Through years of advising, Qube has accrued significant
+                    expertise in ICO strategy, including: token metrics, token
+                    sale mechanism, token distribution, exchange listings, and
+                    more.
+                  </p>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <img src={astsettings} className="max-w-[100px] mx-auto" />
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-primary">
+                    Influencer Network
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Qube has an extensive social-media influencer network that
+                    it utilizes to build a community for its partner blockchain
+                    projects.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
