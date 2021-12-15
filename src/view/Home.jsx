b8d43ca472471e62086cbfbcddd131a5f911f0a1
Ashfahan <me@ashfahan.com>
2021-12-15 05:13:43 +0500

    add work section

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -4,12 +4,14 @@ import Hero from "../section/Hero";
 import Portfolio from "../section/Portfolio";
 import Stake from "../section/Stake";
 import Staking from "../section/Staking";
+import Work from "../section/Work";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <div>
+        <Work />
         <Stake />
         <Portfolio />
         <Staking />
