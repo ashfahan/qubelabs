8c5da83c16af062a4c60392aa05f4931c38a1475
Ashfahan <me@ashfahan.com>
2021-12-16 05:50:17 +0500

    revamp Portfolio section

--- src/section/Portfolio.jsx
+++ src/section/Portfolio.jsx
@@ -1,71 +1,82 @@
 import React from "react";
 
-import box from "../assets/ast/box.png";
+import Group from "../assets/Group.png";
 import bounty from "../assets/ast/bounty.png";
 import bunty from "../assets/ast/bunty.png";
 import electrifyasia from "../assets/ast/electrifyasia.png";
 
 export default function Portfolio() {
   return (
-    <section class="staking-wrapper  " id="portfolio">
-      <img class="bg-icons3" src={box} alt="" />
-      <div class="container">
-        <div class="main-content3 reveal" data-animate-in="up">
-          <div class="work-heading text-center">
-            <h1>Invest with us</h1>
-            <p class="mt-3 staking-inner-text ">
-              These are some of the brands that trust us.
-            </p>
-          </div>
-          <div class="card-group card-group-wrapper ">
-            <div class="card card-1 text-center ">
-              <img
-                src={bounty}
-                class="card-img-top pt-4 card-custom-top1"
-                alt="..."
-              />
-              <div class="card-body text-center">
-                <h5 class="card-title invest-title-text">Bounty Ox</h5>
-                <p class="card-text card-inner-text">
-                  Qube partners with promising blockchain projects, assisting
-                  them towards successful ICOs, through funding, advisory
-                  services and community awareness generation.
-                </p>
+    <section className="mt-24 overflow-x-clip">
+      <div className="container-lg relative">
+        <img
+          className="absolute top-0 right-10 -z-10 w-auto transform translate-x-2/4 translate-y-96"
+          src={Group}
+        />
+
+        <div className="container-lg pt-36">
+          <div className="rounded-2xl bg-slate-200 bg-opacity-60 backdrop-blur-sm p-16">
+            <div className="text-center">
+              <h5 className="text-5xl font-bold text-primary">
+                Invest with us
+              </h5>
+              <p className="text-primary-dark font-medium mt-6 mb-12">
+                These are some of the brands that trust us.
+              </p>
+            </div>
+
+            <div className="row">
+              <div className="col w-4/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="h-[100px] text-center flex items-center justify-center">
+                    <img src={bounty} />
+                  </div>
+
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-primary">
+                    Bounty Ox
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Qube partners with promising blockchain projects, assisting
+                    them towards successful ICOs, through funding, advisory
+                    services and community awareness generation.
+                  </p>
+                </div>
+              </div>
+              <div className="col w-4/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="h-[100px] text-center flex items-center justify-center">
+                    <img src={bunty} />
+                  </div>
+
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-primary">
+                    Wabi
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Qube partners with promising blockchain projects, assisting
+                    them towards successful ICOs, through funding, advisory
+                    services and community awareness generation.
+                  </p>
+                </div>
+              </div>
+              <div className="col w-4/12 p-1">
+                <div className="card w-full h-full card-body text-center rounded-lg border-blue-100 hover:border-blue-300 transition-colors border-2">
+                  <div className="h-[100px] text-center flex items-center justify-center">
+                    <img src={electrifyasia} />
+                  </div>
+
+                  <h5 className="text-xl mt-4 mb-3 font-medium text-primary">
+                    Electrify Asia
+                  </h5>
+                  <p className="card-text card-inner-text">
+                    Qube partners with promising blockchain projects, assisting
+                    them towards successful ICOs, through funding,
+                  </p>
+                </div>
               </div>
             </div>
-            <div class="card card-1 ">
-              <img
-                src={bunty}
-                class="card-img-top pt-4 card-custom-top2"
-                alt="..."
-              />
-              <div class="card-body text-center">
-                <h5 class="card-title invest-title-text">Wabi</h5>
-                <p class="card-text card-inner-text">
-                  Qube partners with promising blockchain projects, assisting
-                  them towards successful ICOs, through funding, advisory
-                  services and community awareness generation.
-                </p>
-              </div>
+            <div className="mt-4 text-center">
+              <div className="btn is-primary px-16">View More</div>
             </div>
-            <div class="card card-1 ">
-              <img
-                src={electrifyasia}
-                class="card-img-top pt-4 card-custom-top3"
-                alt="..."
-              />
-              <div class="card-body text-center">
-                <h5 class="card-title invest-title-text">Electrify Asia</h5>
-                <p class="card-text card-inner-text">
-                  Qube partners with promising blockchain projects, assisting
-                  them towards successful ICOs, through funding, advisory
-                  services and community awareness generation.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="work-heading text-center">
-            <div class="btn-view"> View More</div>
           </div>
         </div>
       </div>
