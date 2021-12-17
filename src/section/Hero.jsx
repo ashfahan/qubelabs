247fc181fea95ff8c3a6265e65c395dc3f1563f6
Ashfahan <me@ashfahan.com>
2021-12-17 14:15:41 +0500

    decrease spacing around section

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -5,7 +5,7 @@ import herovid from "../assets/herovid.mp4";
 export default function Hero() {
   return (
     <section className="flex items-center py-20 md:w-11/12 w-[95%] mx-auto min-h-[75vh] lg:min-h-[auto]">
-      <div className="row items-center">
+      <div className="row items-center w-full">
         <div className="col w-full md:w-6/12">
           <h1 className="text-4xl font-bold leading-[50px]">
             <span className="text-primary">Qube</span> partners with high-potential blockchain
@@ -18,7 +18,7 @@ export default function Hero() {
           <button className="btn is-primary mt-4">Learn more</button>
         </div>
         <div className="col w-6/12 hidden md:block">
-          <video muted autoPlay loop src={herovid}></video>
+          <video className='w-full' muted autoPlay loop src={herovid}></video>
         </div>
       </div>
     </section>
